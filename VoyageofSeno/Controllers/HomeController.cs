72b6ad628cae9aa5713b7e0feafaf36179ab971e
savannagtr <hannah.lat@live.com>
2015-03-25 19:28:23 -0700

    Initial commit

---
+++ VoyageofSeno/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VoyageofSeno.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Story()
+        {
+            return View();
+        }
+        public ActionResult Objective()
+        {
+            return View();
+        }
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+    }
+}
---
+++ VoyageofSeno/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VoyageofSeno.MvcApplication" Language="C#" %>
